ad031fbc6547cc236608bfd2d90620e3f763ec51
Maximo Bautista <max.bautista97@gmail.com>
2019-09-18 02:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let num1;
-let num2;
-
-const multiply = num1 * num;
 
 const random = Math.random(0); 
 
 const mod = 4 / 1;
 
 const max = Math.max(1, 20, 3);
+
+let num1;
+let num2;
+
+const multiply = num1 * num;
