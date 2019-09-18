bdc019ab5ccdf9b662004e7df3eb05ddf700dcb5
Maximo Bautista <max.bautista97@gmail.com>
2019-09-18 02:59:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 62;
+let num2 = 1;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.random(0); 
+
+const mod = 4 / 1;
+
+const max = Math.max(1, 20, 3);
+
+
